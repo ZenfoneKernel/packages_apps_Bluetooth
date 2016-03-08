b9cc2cc4cb4ce8e7751ee3c64dec59b9fde9d111
Martin Rodemann <martin@rodemann.org>
2016-03-08 09:42:59 +0000

    fix merging bug
    Change-Id: I2d8bc00ca63367ab169ad1ac503a7f02b401acc4

--- src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
+++ src/com/android/bluetooth/hfpclient/HeadsetClientStateMachine.java
@@ -453,18 +453,6 @@ final class HeadsetClientStateMachine extends StateMachine {
     private void updateCallSetupIndicator(int callsetup) {
         Log.d(TAG, "updateCallSetupIndicator " + callsetup + " " + mPendingAction.first);
 
-        if (mRingtone != null && mRingtone.isPlaying()) {
-            Log.d(TAG,"stopping ring after no response");
-            mRingtone.stop();
-            if (mAudioManager.getMode() == AudioManager.MODE_RINGTONE) {
-                mAudioManager.setMode(AudioManager.MODE_NORMAL);
-            }
-            //abandon audio focus
-            Log.d(TAG, "abandonAudioFocus");
-            // abandon audio focus after the mode has been set back to normal
-            mAudioManager.abandonAudioFocusForCall();
-        }
-
         if (waitForIndicators(-1, callsetup, -1)) {
             return;
         }
@@ -1011,17 +999,6 @@ final class HeadsetClientStateMachine extends StateMachine {
         int action;
 
         Log.d(TAG, "rejectCall");
-        if ( mRingtone != null && mRingtone.isPlaying()) {
-            Log.d(TAG,"stopping ring after call reject");
-            mRingtone.stop();
-            if (mAudioManager.getMode() == AudioManager.MODE_RINGTONE) {
-                mAudioManager.setMode(AudioManager.MODE_NORMAL);
-            }
-            //abandon audio focus
-            Log.d(TAG, "abandonAudioFocus");
-            // abandon audio focus after the mode has been set back to normal
-            mAudioManager.abandonAudioFocusForCall();
-        }
 
         BluetoothHeadsetClientCall c =
                 getCall(BluetoothHeadsetClientCall.CALL_STATE_INCOMING,
@@ -2042,18 +2019,6 @@ final class HeadsetClientStateMachine extends StateMachine {
         private void processConnectionEvent(int state, BluetoothDevice device) {
             switch (state) {
                 case HeadsetClientHalConstants.CONNECTION_STATE_DISCONNECTED:
-                    if (mRingtone != null && mRingtone.isPlaying()) {
-                        mRingtone.stop();
-                    if (mAudioManager.getMode() ==
-                            AudioManager.MODE_RINGTONE) {
-                        mAudioManager.setMode(AudioManager.MODE_NORMAL);
-                    }
-                    //abandon audio focus
-                    Log.d(TAG, "abandonAudioFocus");
-                    /* abandon audio focus after the mode has
-                     been set back to normal*/
-                    mAudioManager.abandonAudioFocusForCall();
-                    }
                     Log.d(TAG, "Connected disconnects.");
                     // AG disconnects
                     if (mCurrentDevice.equals(device)) {
